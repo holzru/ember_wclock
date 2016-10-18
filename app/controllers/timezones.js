26f8602fbe2e4629c5d3274878f4b0abfe3ffec2
holzru <holzru@gmail.com>
2016-10-17 19:18:52 -0700

    other people are unaccountable as f**k

--- app/controllers/timezones.js
+++ app/controllers/timezones.js
@@ -1,28 +1,31 @@
 import Ember from 'ember';
 
 export default Ember.Controller.extend({
-  init: function() {
-    let timezones = [];
-    for (let i in moment.tz._zones) {
-      timezones.push({
-        name: moment.tz._zones[i].name,
-        offset: moment.tz._zones[i].offsets[0]
-      });
-    }
-    this.set('timezones', timezones);
-    this._super();
-  },
-  selectedTimezone: null,
-  actions: {
-    add: function () {
-      let timezone = this.store.createRecord('timezone', {
-        name: this.get('selectedTimezone').name,
-        offset: this.get('selectedTimezone').offset
-      });
-      timzeone.save();
-    },
-    remove: function(timezone) {
-      timezone.destroyRecord();
-    }
-  }
+    /* create array of timezones with name & offset */
+    init: function() {
+        var timezones = [];
+        for (var i in moment.tz._zones) {
+          timezones.push({
+            name: moment.tz._zones[i].name,
+            offset: moment.tz._zones[i].offsets[0]
+          });
+        }
+        this.set('timezones', timezones);
+        this._super();
+      },
+      selectedTimezone: null,
+      actions: {
+        /* save a timezone record to our offline datastore */
+        add: function() {
+          var timezone = this.store.createRecord('timezone', {
+            name: this.get('selectedTimezone').name,
+            offset: this.get('selectedTimezone').offset
+          });
+          timezone.save();
+        },
+        /* delete a timezone record from our offline datastore */
+        remove: function(timezone) {
+          timezone.destroyRecord();
+        }
+      }
 });
